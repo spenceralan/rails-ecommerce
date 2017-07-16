c4ea4b4d6ca686e7f141af7fe26e2615b11231f7
Spencer Alan <spencer.alan@icloud.com>
2017-07-16 12:53:14 -0700

    show images in product description

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,8 @@
 class Product < ApplicationRecord
   has_many :order_items
 
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.jpg"
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.jpg"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
   validates :name, :description, presence: true
 
