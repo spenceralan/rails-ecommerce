64451fa6fa4dd60ed6c20af1a4a5583472227e38
Spencer Alan <spencer.alan@icloud.com>
2017-07-16 14:10:23 -0700

    add simplecov integration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,7 +13,25 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+require "paperclip/matchers"
+require 'simplecov'
+
+SimpleCov.start
+
 RSpec.configure do |config|
+  # simplecov
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  # Paperclip helpers
+  config.include Paperclip::Shoulda::Matchers
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
