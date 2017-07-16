7a69ebbd1aea6e909531acc30cde2c29e7b1a763
Spencer Alan <spencer.alan@icloud.com>
2017-07-16 12:31:48 -0700

    add paperclip

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,9 @@
 class Product < ApplicationRecord
   has_many :order_items
 
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.jpg"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
   validates :name, :description, presence: true
 
   validates :price,
